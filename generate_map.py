297b7c15d653ba30f4df19e5c36dfd79266414a9
gobbo <robertogobbetti@gmail.com>
2014-07-14 11:19:49 -0400

    fixed bugs in map generation

--- generate_map.py
+++ generate_map.py
@@ -22,6 +22,17 @@ for i in range(len(words)):
 
 words_dict = {words2[i][0]: [words2[i][j] for j in range(1,11)] for i in range(len(words))}
 
+for key in words_dict:
+	for i in range(len(words_dict[key])):
+		s = list(words_dict[key][i])
+		for index, char in enumerate(s):
+			if char == '\'':
+				s[index] = '\\'+'\''
+			elif char == '&':
+				s[index] = '-'
+		words_dict[key][i] = ''.join(s)
+
+
 # Load the SVG map
 svg = open('states.svg', 'r').read()
 # Load into Beautiful Soup
@@ -35,41 +46,40 @@ paths = soup.findAll('path')
 # marker-start:none;stroke-linejoin:bevel;fill:'
 
 all_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', \
-		'Georgia','Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Lousiana', 'Maine', 'Maryland', \
+		'Georgia','Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', \
 		'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', \
 		'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', \
 		'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', \
 		'West Virginia', 'Wisconsin', 'Wyoming']
 
 
-# display = {state: "displayName('"+state+"')" for state in all_states}
-# s = 'New Hampshire' in words_dict.keys()
-
-print str(len(words_dict)) +' '+ str(len(all_states))
-print words_dict['Illinois']
-
-# display = {state: "display1('"+words_dict[state][0]+"'), display2('"+words_dict[state][1]+"'), display3('"+words_dict[state][2]+"'), \
-# 	display4('"+words_dict[state][3]+"'), display5('"+words_dict[state][4]+"'), display6('"+words_dict[state][5]+"'), \
-# 	display7('"+words_dict[state][6]+"'), display8('"+words_dict[state][7]+"'), display9('"+words_dict[state][8]+"'), \
-# 	display10('"+words_dict[state][9]+"')" for state in all_states}
-
-# for state in all_states:
-# 	for p in paths:
-# 		if p['id'] == "path57":
-# 			continue
-# 		elif p['id'] == state:
-# 			p['onmouseover']=display[state]
 
 
+# for i in range(len(words_dict['Kentucky'])):
+# 	print words_dict['Kentucky'][i]
 
-# # Output map
-# new_map = soup.prettify()
+display = {state: "display1('"+words_dict[state][0]+"'), display2('"+words_dict[state][1]+"'), display3('"+words_dict[state][2]+"'), \
+	display4('"+words_dict[state][3]+"'), display5('"+words_dict[state][4]+"'), display6('"+words_dict[state][5]+"'), \
+	display7('"+words_dict[state][6]+"'), display8('"+words_dict[state][7]+"'), display9('"+words_dict[state][8]+"'), \
+	display10('"+words_dict[state][9]+"')" for state in all_states}
 
-# #print new_map
+for state in all_states:
+	for p in paths:
+		if p['id'] == "path57":
+			continue
+		elif p['id'] == state:
+			p['onmouseover']=display[state]
 
-# f = open('states.svg', 'w')
-# f.write(new_map)
-# f.close()
+
+
+# Output map
+new_map = soup.prettify()
+
+#print new_map
+
+f = open('states.svg', 'w')
+f.write(new_map)
+f.close()
 
 
 
